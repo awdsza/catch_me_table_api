ce1634020d8cea0afc71ac0b31771fba93e5fbdb
awdsza <37355034+awdsza@users.noreply.github.com>
2024-01-15 20:52:12 +0900

    feat:최초 프로젝트 커밋

---
+++ src/main/java/com/app/catchmetable/CatchmetableApplication.java
@@ -0,0 +1,13 @@
+package com.app.catchmetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchmetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchmetableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/catchmetable/CatchmetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.catchmetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchmetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
