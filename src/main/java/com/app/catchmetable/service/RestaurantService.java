8b0918fbd4c793522cb8ce231164df54a4e207eb
awdsza <37355034+awdsza@users.noreply.github.com>
2024-03-28 08:41:38 +0900

    rename:예외 클래스명 변경으로인한 수정

--- src/main/java/com/app/catchmetable/service/RestaurantService.java
+++ src/main/java/com/app/catchmetable/service/RestaurantService.java
@@ -7,7 +7,7 @@ import com.app.catchmetable.dto.RestaurantUpdateRequestDto;
 import com.app.catchmetable.dto.RestaurantRequestDto;
 import com.app.catchmetable.exception.DuplicateUserException;
 import com.app.catchmetable.exception.LoginFailException;
-import com.app.catchmetable.exception.NotExistRestaurantException;
+import com.app.catchmetable.exception.NotExistUserException;
 import com.app.catchmetable.repository.RestaurantRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -46,7 +46,7 @@ public class RestaurantService {
     public Restaurant findRestaurant(Long restaurant_id){
         Restaurant restaurant = restaurantRepository.findRestaurant(restaurant_id);
         if(restaurant == null){
-            throw new NotExistRestaurantException("해당 ID를 가진 레스토랑이 존재하지않습니다.");
+            throw new NotExistUserException("해당 ID를 가진 레스토랑이 존재하지않습니다.");
         }
         return restaurant;
     }
